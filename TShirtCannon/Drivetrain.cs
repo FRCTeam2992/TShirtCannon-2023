b6ec7c93ce1393ca891bfad121b42a52b1ad12f6
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-09-06 21:03:43 -0500

    Implement better deadbanding and smoothing for drive sticks based on feedback

--- TShirtCannon/Drivetrain.cs
+++ TShirtCannon/Drivetrain.cs
@@ -35,8 +35,8 @@ namespace TShirtCannon2023.Subsystems
             float twist = gamepad.GetAxis((uint)twistAxis);
 
             /* Deadband gamepad axis inputs */
-            forward = deadband(forward, DrivetrainConstants.DEADBAND_WIDTH);
-            twist = deadband(twist, DrivetrainConstants.DEADBAND_WIDTH);
+            forward = deadband(forward, DrivetrainConstants.DEADBAND_THRESHOLD);
+            twist = deadband(twist, DrivetrainConstants.DEADBAND_THRESHOLD);
 
             /* Compute throttles */
             float[] throttles = computeThrottle(forward, twist);
@@ -52,35 +52,66 @@ namespace TShirtCannon2023.Subsystems
         }
 
         /**
-         * If value is within 10% of center, clear it.
+         * If value is within threshold of center, clear it.
          * @param value [out] floating point value to deadband.
          */
-        private float deadband(float value, float width)
+        private float deadband(float value, float threshold)
         {
-            if (value < -width)
+            // Force Limit from -1.0 to 1.0
+            value = (float)Math.Max(-1, Math.Min(1, value));
+
+            // Check If Value is Inside the Deadzone
+            if (value >= 0.0)
             {
-                /* outside of deadband */
-                return value;
-            }
-            else if (value > +width)
-            {
-                /* outside of deadband */
-                return value;
+                if (Math.Abs(value) >= threshold)
+                {
+                    return (value - threshold) / (1 - threshold);
+                }
+                else
+                {
+                    return (float)0.0;
+                }
             }
             else
             {
-                /* within the percent width so zero it */
-                return (float)0.0;
+                if (Math.Abs(value) >= threshold)
+                {
+                    return (value + threshold) / (1 - threshold);
+                }
+                else
+                {
+                    return (float)0.0;
+                }
             }
         }
 
-        /* Compute an array of smoothed (via squaring and constant curvature
-         * throttles for left and right drive motors */
+        /* Smooth an input value via mixing linear and cubic gain
+         * curves. See the following reference for more detail:
+         * https://www.chiefdelphi.com/t/paper-joystick-sensitivity-gain-adjustment/107280
+         */
+        private float smoothInput(float x)
+        {
+            float a = DrivetrainConstants.STICK_GAIN_FACTOR;
+            float b = DrivetrainConstants.STICK_INV_DEADBAND;
+
+            if (x > 0.0)
+            {
+                return (float)(b + (1.0 - b) * (a * x * x * x + (1.0 - a) * x));
+            }
+            if (x < 0.0)
+            {
+                return (float)(-b + (1.0 - b) * (a * x * x * x + (1.0 - a) * x));
+            }
+            return (float)0.0;
+        }
+
+        /* Compute an array of smoothed throttles for left 
+         * and right drive motors */
         private float[] computeThrottle(float forwardAxis, float twistAxis)
         {
-            /* Square inputs to smooth low-speed drive */
-            float forward = forwardAxis * forwardAxis * (float)Math.Sign(forwardAxis);
-            float twist = twistAxis * twistAxis * (float)Math.Sign(twistAxis);
+            /* Smooth inputs for better control at low-speed drive */
+            float forward = smoothInput(forwardAxis);
+            float twist = smoothInput(twistAxis);
 
             /* Compute throttle for each side of the robot
              * via constant-curvature drive */
